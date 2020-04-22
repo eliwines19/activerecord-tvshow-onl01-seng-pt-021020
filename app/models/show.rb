e60c074514bd4334e102b9e394ed60ee7534a670
Elijah wines <elijahwines84@yahoo.com>
2020-04-22 16:34:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating #return highest value in the ratings column, use 'maximum'
-    Show.maximum[:rating]
+    Show.maximum(:rating)
   end
 
   def self.most_popular_show #returns the show with the highest rating
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating #returns the lowest value in the ratings column
-    Show.minimun[:rating]
+    Show.minimun(:rating)
   end
 
   def self.least_popular_show #returns the show with the lowest rating
@@ -17,7 +17,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.ratings_sum #returns the sum of all the ratings
-    Show.sum[:rating]
+    Show.sum(:rating)
   end
 
   def self.popular_shows #returns an array of all the shows that have a rating higher than 5, use 'where'
