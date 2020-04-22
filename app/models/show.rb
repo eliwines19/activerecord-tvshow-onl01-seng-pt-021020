1e76ac1444be0382efc28ed0a524b601dd532f16
Elijah wines <elijahwines84@yahoo.com>
2020-04-22 16:25:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,11 @@
 class Show < ActiveRecord::Base
 
   def highest_rating #return highest value in the ratings column, use 'maximum'
+    Show.maximum[:rating]
   end
 
   def most_popular_show #returns the show with the highest rating
+    Show.where("rating = ?", self.highest_rating).first
   end
 
   def lowest_rating #returns the lowest value in the ratings column
